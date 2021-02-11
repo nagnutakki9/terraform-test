d8939528fdde5ca8ac165ab8c99e453b07e68eb9
Nutakkinani <53839996+Nutakkinani@users.noreply.github.com>
2021-02-12 00:00:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.ssh_permit
+    cidr_blocks = var.cidr
   }
 
   egress {
